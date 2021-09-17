a76bb2082716bc4786f996b8df91f7471eea9e53
kuriki <KyrelJerome@Users.noreply.github.com>
2021-09-17 11:37:56 -0400

    Didnt stage previous commit xd

---
+++ client/grid_player.py
@@ -0,0 +1,10 @@
+from helper_classes import *
+
+class GridPlayer:
+
+    def __init__(self):
+        self.foo = True
+
+    def tick(self, game_map: Map, your_units: Units, enemy_units: Units,
+             resources: int, turns_left: int) -> [Move]:
+        return []
