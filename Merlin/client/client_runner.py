d548ac38b87b3252c7e6eabf64a8416af3df906c
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2022-01-02 23:56:11 -0500

    add merlin launcher

--- Merlin/client/client_runner.py
+++ Merlin/client/client_runner.py
@@ -1,6 +1,15 @@
-class MerlinClientLauncher:
-    @staticmethod
-    def start():
-        pass
+from Engine.client.client_launcher import ClientLauncher
 
-MerlinClientLauncher.start()
+from .grid_player import GridPlayer
+class MerlinClientLauncher(ClientLauncher):
+    '''
+        A launch system for the Merlin game Client
+    '''
+    def getDecodeDataFactory(self):
+        return MerlinTileFactory() #TODO
+
+    def getEncodeDataFactory(self):
+        return MerlinMoveFactory() #TODO
+
+    def getPlayer(self):
+        return GridPlayer()
