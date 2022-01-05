06ce6635e35a5bc3fff3f29e832ca6a6fd3625a4
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2022-01-05 18:03:43 -0500

    add verify moves

--- Merlin/server/move.py
+++ Merlin/server/move.py
@@ -4,10 +4,7 @@ from .unit import WorkerUnit
 
 from Engine.server.move import Move
 
-from game.constants import Moves, Units
-
-# 0 represents cost to buy, anything above represents cost to upgrade
-
+from game.constants import Moves, Units, UPGRADE_COSTS, Direction, MAX_LEVEL
 class MerlinMoveFactory:
     def createMove(uid:str, *restInfo):
         '''
@@ -37,12 +34,6 @@ class GameMove(Move):
       The subclass should implement this
       '''
       pass
-  def verifyMove(self):
-      '''
-      Returns whether the move is valid
-      The subclass should implement this
-      '''
-      pass
   def makeMove(self, gameState): #Note that this should take in a game state
       '''
       Perform move on the game state
@@ -60,11 +51,7 @@ class AttackMove(GameMove):
       Returns the serialized form of this move
       '''
       return {'command': Moves.ATTACK, "unit": self.unit, "target": self.target }
-  def verifyMove(self):
-      '''
-      Returns whether the move is valid
-      '''
-      pass
+
   def makeMove(self, gameState): #Note that this should take in a game state
       '''
       Perform move on the game state
@@ -80,11 +67,6 @@ class UpgradeMove(GameMove):
       Returns the serialized form of this move
       '''
       return {'command': Moves.UPGRADE, "unit": self.unit}
-  def verifyMove(self):
-      '''
-      Returns whether the move is valid
-      '''
-      pass
   def makeMove(self, gameState): #Note that this should take in a game state
       '''
       Perform move on the game state
@@ -100,11 +82,7 @@ class DirectionMove(GameMove):
       Returns the serialized form of this move
       '''
       return {'command': Moves.DIRECTION, "unit": self.unit, "direction": self.direction}
-  def verifyMove(self):
-      '''
-      Returns whether the move is valid
-      '''
-      pass
+
   def makeMove(self, gameState): #Note that this should take in a game state
       '''
       Perform move on the game state
@@ -120,22 +98,7 @@ class MineMove(GameMove):
         Returns the serialized form of this move
         '''
         return {'command': Moves.MINE, "unit": self.unit }
-    def verifyMove(self, grid):
-        '''
-        Returns whether the move is valid
-        '''
-        k = self.unit.id
-        if isinstance(self.unit, WorkerUnit):
-            if self.unit.is_duplicating():
-                print('ERROR: {} cannot act while duplicating'.format(k))
-                return False
 
-            if self.unit.is_mining():
-                print('ERROR: {} cannot act while mining'.format(k))
-                return False
-            if self.grid[self.unit.x][self.unit.y].repr() in []:
-                return 
-        return True
     def makeMove(self, gameState): #Note that this should take in a game state
         '''
         Perform move on the game state
@@ -143,8 +106,8 @@ class MineMove(GameMove):
         pass
 
 class BuyMove(GameMove):
-  def __init__(self, unitType, direction):
-      super().__init__(None)
+  def __init__(self, unit, unitType, direction):
+      super().__init__(unit)
       self.unitType = unitType
       self.direction = direction
   
@@ -153,11 +116,7 @@ class BuyMove(GameMove):
       Returns the serialized form of this move
       '''
       return {'command': Moves.BUY, "unitType": self.unitType, 'direction': self.direction}
-  def verifyMove(self):
-      '''
-      Returns whether the move is valid
-      '''
-      pass
+
   def makeMove(self, gameState): #Note that this should take in a game state
       '''
       Perform move on the game state
@@ -173,11 +132,6 @@ class CaptureMove(GameMove):
       Returns the serialized form of this move
       '''
       return {'command': Moves.CAPTURE, "unit": self.unit, 'direction': self.direction}
-  def verifyMove(self):
-      '''
-      Returns whether the move is valid
-      '''
-      pass
   def makeMove(self, gameState): #Note that this should take in a game state
       '''
       Perform move on the game state
